3e9a10a7d6456d26151209efb4fc6b2e1c05fcff
dobrez <dejan.obrez@gmail.com>
2024-06-16 22:41:32 +0200

    addad runtime instantiation and ScoreBoard.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 1;
+
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        // go look trough the entire object and the first scoreboar you find thats the one we reffering to.
+        // it is resource intensive, so we dont want to use it often. We should not at all use it in Update();
+        scoreBoard = FindObjectOfType<ScoreBoard>(); 
+
+    }
+
+
     void OnParticleCollision(GameObject other)
     {
-        Debug.Log("enemy hit");
+
+        GameObject vfx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;  
         Destroy(gameObject);
     }
 
