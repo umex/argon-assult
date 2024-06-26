24da4e5695f90647bc6aca7522d74dd2ef247903
dobrez <dejan.obrez@gmail.com>
2024-06-26 22:27:14 +0200

    Finding parent pragmatically

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,20 +6,29 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] GameObject hitVFX;
-    [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
     [SerializeField] int hitPoints = 1;
 
 
     ScoreBoard scoreBoard;
+    GameObject parentGameObject;
 
     void Start()
     {
         // go look trough the entire object and the first scoreboar you find thats the one we reffering to.
         // it is resource intensive, so we dont want to use it often. We should not at all use it in Update();
-        scoreBoard = FindObjectOfType<ScoreBoard>(); 
-
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("SpawnAtRuntime");
+        AddRigidbody();
     }
+
+    void AddRigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+
 
 
     void OnParticleCollision(GameObject other)
@@ -34,7 +43,7 @@ public class Enemy : MonoBehaviour
     void ProcessHit()
     {
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         hitPoints--;
         scoreBoard.IncreaseScore(scorePerHit);
     }
@@ -43,7 +52,7 @@ public class Enemy : MonoBehaviour
     {
         
         GameObject fx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        fx.transform.parent = parent;
+        fx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
     }
 
